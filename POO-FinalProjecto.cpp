a158bd829be29970833717b214e9c0539ee70b0f
almaazann <A01715142@tec.mx>
2026-06-14 20:43:44 -0600

    New classes, a expample for main and more
    I created more classes like:
    
    Cita with operator overloading, expediente, with rule of three, Paciente and Clinica which contains everyone, i also did an example for main, almost completed the project

--- POO-FinalProjecto.cpp
+++ POO-FinalProjecto.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include<memory>
+#include <cstring>
 using namespace std;
 
 class Medicamento{
@@ -136,3 +138,201 @@ ostream& operator<<(ostream& os, const Cita& c){
         return os;
     }
 
+class Expediente{
+    public: 
+    char* historial;
+    size_t len;
+    string Estado_salud;
+    Doctor* miDoc;
+
+    void allocCopy(const char* h, size_t n){
+        historial = new char[n + 1];
+        memcpy(historial, h, n + 1);
+        len = n;
+    }
+
+    Expediente(char* h, string es, Doctor* d) : Estado_salud(es), miDoc(d){
+        allocCopy(h, strlen(h));
+        cout << "Expediente incializado" << endl;
+    }
+
+    Expediente() : Estado_salud("Estable"), miDoc(nullptr){
+        allocCopy("N", strlen("N"));
+        cout << "Expediente inicizializado" << endl;
+    }
+
+    void show_info(){
+        cout << "Su estado de salud es: " << Estado_salud << endl;
+        miDoc->info_doctor();
+        cout << "Con un historial de: " << historial << endl;
+    }
+
+    ~Expediente(){
+        cout << "Expediente terminado" << endl;
+        delete[] historial;
+    }
+
+    Expediente(const Expediente& other){
+        Estado_salud = other.Estado_salud;
+        miDoc = other.miDoc;
+        allocCopy(other.historial, other.len);
+    }
+
+    Expediente& operator=(const Expediente& rhs){
+        if(this== &rhs) return *this;
+        delete[] historial;
+        allocCopy(rhs.historial, rhs.len);
+        Estado_salud = rhs.Estado_salud;
+        miDoc = rhs.miDoc;
+        return *this;
+    }
+};
+
+class Paciente{
+    private:
+    int edad;
+    int id;
+    string nombre;
+    Expediente miExpediente;
+
+    public:
+
+    Paciente(int e, int i, string n, const Expediente& miex) : edad(e), id(i), nombre(n), miExpediente(miex){
+        cout << "Paciente registrado" << endl;
+    }
+
+    Paciente() : edad(0), id(1234), nombre("Regular dude"), miExpediente(){
+        cout << "Paciente registrado" << endl;
+    }
+
+    ~Paciente(){
+        cout << "Paciente dado de alta" << endl;
+    }
+
+    void mostrar_info(){
+        cout << "Paciente de nombre: " << nombre << endl;
+        cout << "Con edad de: " << edad << " años" << endl;
+        cout << "ID de: " << id << endl;
+        miExpediente.show_info();
+    }
+
+};
+
+class Clinica{
+private:
+    vector<unique_ptr<Personal>> personal;
+    vector<Paciente> pacientes;
+    vector<Cita> citas;
+
+public:
+
+    Clinica(){
+        cout << "Clinica abierta" << endl;
+    }
+
+    ~Clinica(){
+        cout << "Clinica cerrada" << endl;
+    }
+
+    void agregarPersonal(unique_ptr<Personal> p){
+        personal.push_back(move(p));
+    }
+
+    void agregarPaciente(const Paciente& p){
+        pacientes.push_back(p);
+    }
+
+    void agregarCita(const Cita& c){
+        citas.push_back(c);
+    }
+
+    void mostrarPersonal() const{
+        cout << "\nEl personal es: \n";
+
+        for(const auto& p : personal){
+            p->tratar_paciente();
+        }
+    }
+
+    void mostrarCitas() const{
+        cout << "\nLas citas son: \n";
+
+        for(const auto& c : citas){
+            cout << c << endl;
+        }
+    }
+
+    void mostrarPacientes(){
+        cout << "\nActualmente los pacientes son: \n";
+
+        for(auto& p : pacientes){
+            p.mostrar_info();
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+
+    Clinica miClinica;
+
+    auto doc1 = make_unique<Doctor>(
+        101,
+        "Juan Perez",
+        "Medicina",
+        "cardiologo"
+    );
+
+    auto rec1 = make_unique<Recepcionista>(
+        201,
+        "Maria Lopez",
+        "Recepcion",
+        "Matutino"
+    );
+
+    Doctor* ptrDoc = doc1.get();
+
+    miClinica.agregarPersonal(move(doc1));
+    miClinica.agregarPersonal(move(rec1));
+
+
+    Expediente exp1(
+        (char*)"Hipertension",
+        "Estable",
+        ptrDoc
+    );
+
+    Paciente pac1(
+        45,
+        5001,
+        "Carlos Ruiz",
+        exp1
+    );
+
+    miClinica.agregarPaciente(pac1);
+
+    Cita cita1(
+        "20/06/2026 10:00",
+        ptrDoc
+    );
+
+    miClinica.agregarCita(cita1);
+
+    cout << "\n=========================\n";
+
+    ptrDoc->tratar_paciente();
+
+    cout << "\n=========================\n";
+
+    miClinica.mostrarPacientes();
+
+    cout << "\n=========================\n";
+
+    miClinica.mostrarCitas();
+
+    cout << "\n=========================\n";
+
+    miClinica.mostrarPersonal();
+
+    return 0;
+}
